43e6a88e8c4d2ed4b447dc8e73031f0e926d66b4
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-10-01 21:10:12 -0300

    Retirando erro do lint 2

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
 
